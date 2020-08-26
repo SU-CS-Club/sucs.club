d0cf0cc71caeb9b8b91599105c9a807f6320103b
Jake Gutierrez <gutierr8@southwestern.edu>
2020-08-26 12:36:01 -0500

    Update build script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ BASE_URL="https://angry-benz-029552.netlify.app"
 git submodule update --init --recursive
 
 cd notes
-hugo --gc --minify -b="$BASE_URL/notes" --destination="../public/notes"
+hugo --gc --minify -b="$BASE_URL/notes/" --destination="../public/notes"
 
 cd ../www
 hugo --gc --minify -b="$BASE_URL/" --destination="../public"
